47f8c088c44ff821b56d34b9787995c8d8b2429d
Minh Đức <minhducchipbom@gmail.com>
2020-09-18 19:08:57 +0700

    Initial Commit

--- Homework1/Celsius_to_Fahrenheit.py
+++ Homework1/Celsius_to_Fahrenheit.py
@@ -1,6 +1,6 @@
-C = 0
-F = 0
+# Celsius_to_Fahrenheit_temperature_Converter
+
 print('Enter Celsius temperature:')
 C = float(input().strip())
-F = (((9/5)*C)+32)
+F = (((9 / 5) * C) + 32)
 print('The Fahrenheit temperature: ', F)
