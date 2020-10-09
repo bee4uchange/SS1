67384e9b0c5d62d6f3c6c28a706c687fc989db9e
Minh Đức <minhducchipbom@gmail.com>
2020-10-09 09:21:45 +0700

    thi bang lai xe

--- B_Day/Week3/Driver_License.py
+++ B_Day/Week3/Driver_License.py
@@ -4,43 +4,62 @@ def main():
     cor = []
     ans = []
 
+    # READ DATA FROM FILE
     with open('Ques.txt', encoding='utf-8-sig', mode='r') as f:
         ques = f.readlines()
     ques = [x.strip() for x in ques]
-
-    with open('Ques2.txt', encoding='utf-8-sig', mode='r') as f:
-        ques_choice = f.readlines()
-    ques_choice = [x.strip() for x in ques_choice]
 
     with open('Ans.txt', encoding='utf-8-sig', mode='r') as f:
         cor = f.readlines()
     cor = [x.strip() for x in cor]
 
+    # PRINT QUESTION AND INPUT ANSWER
     check = ['A', 'B', 'C', 'D']
+    total = 1
     for i in range(len(ques)):
         print(ques[i])
-        print(ques_choice[i])
-        while True:
-            in_ = input('Câu trả lời của bạn: ')
-            in_ = in_.upper()
-            if in_.isalpha() == False:
-                print('Điền A,B,C hoặc D')
-                continue
-            elif not in_ in check:
-                print('Điền A,B,C hoặc D')
-                continue
-            else:
-                break
-        ans.append(in_)
-        print()
+        if ques[i] == '':
+            total += 1
+            while True:
+                in_ = input('Câu trả lời của bạn: ')
+                in_ = in_.upper()
+                if in_.isalpha() == False:
+                    print('Điền A,B,C hoặc D')
+                    continue
+                elif not in_ in check:
+                    print('Điền A,B,C hoặc D')
+                    continue
+                else:
+                    break
+            ans.append(in_)
+            print()
+        else:
+            continue
+    print()
+    while True:
+        in_ = input('Câu trả lời của bạn: ')
+        in_ = in_.upper()
+        if in_.isalpha() == False:
+            print('Điền A,B,C hoặc D')
+            continue
+        elif not in_ in check:
+            print('Điền A,B,C hoặc D')
+            continue
+        else:
+            break
+    ans.append(in_)
+    print()
+    print(ans)
+    # COMPARE ANSWER WITH KEY
     total_num_wr = 0
     ques_wrong = []
-    for i in range(len(ques)):
-        if ans[i] != cor[i]:
+    for i in range(total):
+        if ans[i] != cor[i][-1:]:
             total_num_wr += 1
             ques_wrong.append(i + 1)
 
-    total_num_cor = len(ques) - total_num_wr
+    # CALCULATE TOTAL SCORE AND EVALUATE
+    total_num_cor = total - total_num_wr
     print('Số câu đúng là:', total_num_cor)
     print('Số câu sai là:', total_num_wr)
     print('Các câu sai:', ques_wrong)
